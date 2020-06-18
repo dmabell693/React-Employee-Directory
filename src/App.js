62532605fb6e9c7143908c226f085b05bcdcf41d
Daniel Abell <da450@cairn.edu>
2020-06-18 15:17:08 -0400

    added filter by role functionality

--- src/App.js
+++ src/App.js
@@ -26,16 +26,106 @@ class App extends React.Component {
   //   let deptArr = this.state.employees.filter(employee => employee.department === department);
   //   this.setState({employees: deptArr});
   // }
+
+  //filter by department functions
+  filterManagement = () => {
+    let manArr = this.state.employees.filter(employee => employee.department === "Management");
+    this.setState({employees: manArr});
+  }
+
   filterSales = () => {
     let salesArr = this.state.employees.filter(employee => employee.department === "Sales");
     this.setState({employees: salesArr});
   }
+
+  filterAccounting = () => {
+    let accArr = this.state.employees.filter(employee => employee.department === "Accounting");
+    this.setState({employees: accArr});
+  }
+
+  filterSupplierRel = () => {
+    let suppRelArr = this.state.employees.filter(employee => employee.department === "Supplier Relations");
+    this.setState({employees: suppRelArr});
+  }
+
+  filterQualityAss = () => {
+    let qualityAss = this.state.employees.filter(employee => employee.department === "Quality Assurance");
+    this.setState({employees: qualityAss});
+  }
+
+  filterReception = () => {
+    let recArr = this.state.employees.filter(employee => employee.department === "Reception");
+    this.setState({employees: recArr});
+  }
+
+  filterCustRel = () => {
+    let custRelArr = this.state.employees.filter(employee => employee.department === "Customer Relations");
+    this.setState({employees: custRelArr});
+  }
+
+  filterIntern = () => {
+    let internArr = this.state.employees.filter(employee => employee.department === "Intern");
+    this.setState({employees: internArr});
+  }
+
+  filterWarehouse = () => {
+    let warehouseArr = this.state.employees.filter(employee => employee.department === "Warehouse");
+    this.setState({employees: warehouseArr});
+  }
+
+  filterHR = () => {
+    let hrArr = this.state.employees.filter(employee => employee.department === "Human Resources");
+    this.setState({employees: hrArr});
+  }
   
+  //filter by role functions
+  filterRegMan = () => {
+    let regManArr = this.state.employees.filter(employee => employee.role === "Regional Manager");
+    this.setState({employees: regManArr});
+  }
+
+  filterSalesLead = () => {
+    let salesLeadArr = this.state.employees.filter(employee => employee.role === "Sales Lead");
+    this.setState({employees: salesLeadArr});
+  }
+
+  filterSalesperson = () => {
+    let salespersonArr = this.state.employees.filter(employee => employee.role === "Salesperson");
+    this.setState({employees: salespersonArr});
+  }
+
+  filterLeadAccountant = () => {
+    let leadAccountantArr = this.state.employees.filter(employee => employee.role === "Lead Accountant");
+    this.setState({employees: leadAccountantArr});
+  }
+
+  filterAccountant = () => {
+    let accountantArr = this.state.employees.filter(employee => employee.role === "Accountant");
+    this.setState({employees: accountantArr});
+  }
+
   filterRepresentative = () => {
     let repArr = this.state.employees.filter(employee => employee.role === "Representative");
     this.setState({employees: repArr});
   }
 
+  filterThatsRight = () => {
+    let thatsRightArr = this.state.employees.filter(employee => employee.role === "That's Right");
+    this.setState({employees: thatsRightArr});
+  }
+
+  filterReceptionist = () => {
+    let receptionistArr = this.state.employees.filter(employee => employee.role === "Receptionist");
+    this.setState({employees: receptionistArr});
+  }
+
+  filterWarehouseManager = () => {
+    let wareManArr = this.state.employees.filter(employee => employee.role === "Warehouse Manager");
+    this.setState({employees: wareManArr});
+  }
+
+
+  //sort functions
   sortFirstName = () => {
     let sortFirstNameArr = this.state.employees.sort((a, b) => (a.first_name > b.first_name) ? 1 : -1);
     this.setState({employees: sortFirstNameArr});
@@ -70,8 +160,30 @@ class App extends React.Component {
     return (
       <Wrapper 
       // filterDepartment={() => this.filterDepartment(this.props.criteria)}
+      //Departments
+      filterManagement={() => this.filterManagement()}
       filterSales={() => this.filterSales()}
+      filterAccounting={() => this.filterAccounting()}
+      filterSupplierRel={() => this.filterSupplierRel()}
+      filterQualityAss={() => this.filterQualityAss()}
+      filterReception={() => this.filterReception()}
+      filterCustRel={() => this.filterCustRel()}
+      filterIntern={() => this.filterIntern()}
+      filterWarehouse={() => this.filterWarehouse()}
+      filterHR={() => this.filterHR()}
+
+      //Roles
+      filterRegMan={() => this.filterRegMan()}
+      filterSalesLead={() => this.filterSalesLead()}
+      filterSalesperson={() => this.filterSalesperson()}
+      filterLeadAccountant={() => this.filterLeadAccountant()}
+      filterAccountant={() => this.filterAccountant()}
       filterRepresentative={() => this.filterRepresentative()}
+      filterThatsRight={() => this.filterThatsRight()}
+      filterReceptionist={() => this.filterReceptionist()}
+      filterWarehouseManager={() => this.filterWarehouseManager()}
+
+      //Sort
       resetForm={() => this.resetForm()}
       sortFirstName={() => this.sortFirstName()}
       sortLastName={() => this.sortLastName()}
--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -4,13 +4,16 @@ import "./style.css";
 function Wrapper(props) {
     return (
         <div className="container">
+            <header className="row">
+                <h1 className="col text-center">Employee Directory</h1>
+            </header>
             <div className="row">
                 {/* <button className="btn btn-primary" onClick={props.filterDepartment} datatype={props.criteria} >
                 Filter by Department
                 </button> */}
-                <div className="dropdown col">
+                <div className="dropdown">
                     <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Sort by:
+                        Sort
                     </button>
                     <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
                         <button className="dropdown-item" onClick={props.sortFirstName}>First Name</button>
@@ -23,13 +26,48 @@ function Wrapper(props) {
                 </div>
 
 
-                <div className="dropdown col">
+                <div className="dropdown">
                     <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Filter by Department
+                        Find by Department
                     </button>
                     <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <button className="dropdown-item" onClick={props.filterManagement}>Management</button>
                         <button className="dropdown-item" onClick={props.filterSales}>Sales</button>
-                        <button className="dropdown-item" onClick={props.filterRepresentative}>Filter by Representative</button>
+                        <button className="dropdown-item" onClick={props.filterAccounting}>Accounting</button>
+                        <button className="dropdown-item" onClick={props.filterSupplierRel}>Supplier Relations</button>
+                        <button className="dropdown-item" onClick={props.filterQualityAss}>Quality Assurance</button>
+                        <button className="dropdown-item" onClick={props.filterReception}>Reception</button>
+                        <button className="dropdown-item" onClick={props.filterCustRel}>Customer Relations</button>
+                        <button className="dropdown-item" onClick={props.filterIntern}>Intern</button>
+                        <button className="dropdown-item" onClick={props.filterWarehouse}>Warehouse</button>
+                        <button className="dropdown-item" onClick={props.filterHR}>Human Resources</button>
+                        <button className="dropdown-item" onClick={props.resetForm}>Reset Table</button>
+                    </div>
+                </div>
+
+                <div className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Find by Role
+                    </button>
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <button className="dropdown-item" onClick={props.filterRegMan}>Regional Manager
+                        </button>
+                        <button className="dropdown-item" onClick={props.filterSalesLead}>Sales Lead
+                        </button>
+                        <button className="dropdown-item" onClick={props.filterSalesperson}>Salesperson
+                        </button>
+                        <button className="dropdown-item" onClick={props.filterLeadAccountant}>Lead Accountant
+                        </button>
+                        <button className="dropdown-item" onClick={props.filterAccountant}>Accountant
+                        </button>
+                        <button className="dropdown-item" onClick={props.filterRepresentative}>Representative
+                        </button>
+                        <button className="dropdown-item" onClick={props.filterThatsRight}>That's Right
+                        </button>
+                        <button className="dropdown-item" onClick={props.filterReceptionist}>Receptionist
+                        </button>
+                        <button className="dropdown-item" onClick={props.filterWarehouseManager}>Warehouse Manager
+                        </button>
                         <button className="dropdown-item" onClick={props.resetForm}>Reset Form</button>
                     </div>
                 </div>
